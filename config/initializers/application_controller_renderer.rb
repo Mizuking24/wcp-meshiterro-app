7991a9ec5fdaa19f8218910488fd50037990bde7
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-06-22 16:28:58 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f8c4a55ba186f22adee890430a19e96fa1f898f14f3ab40b15bbf3a9e5882b0c8326787351f55003cdd36da676ce0aa39a163ec60cb28cadeda882c0076a8249'
